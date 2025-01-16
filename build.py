3e1462e6cd5870d42b4e52b647159e2c2c5603d5
zekroTJA <riho@live.de>
2025-01-16 13:23:41 +0100

    add watch mode to build script

--- build.py
+++ build.py
@@ -1,14 +1,9 @@
+import argparse
 import datetime
+import logging
 import os
 import shutil
-
-
-SRC_DIR = "src"
-TEMPLATE_DIR = os.path.join(SRC_DIR, "templates")
-PUBLIC_DIR = os.path.join(SRC_DIR, "public")
-PAGES_DIR = os.path.join(SRC_DIR, "pages")
-
-OUTPUT_DIR = "dist"
+import watchfiles
 
 
 NAV_OVERRIDES = {
@@ -22,28 +17,68 @@ OUTPUT_OVERRIDES = {
 }
 
 
+def parse_args():
+    p = argparse.ArgumentParser()
+
+    p.add_argument("-w", "--watch", action='store_true',
+                   help="Watch source directory for changes and rebuild")
+    p.add_argument("-s", "--source", default="src",
+                   help="Source directory")
+    p.add_argument("-o", "--output", default="dist",
+                   help="Output directory")
+    p.add_argument("-l", "--log-level", default=2, type=int,
+                   help="Set log level of the default logger. "
+                   "(default 1)")
+
+    return p.parse_args()
+
+
 def main():
+    args = parse_args()
 
-    if os.path.exists(OUTPUT_DIR):
-        shutil.rmtree(OUTPUT_DIR)
+    logging.basicConfig(
+        level=(args.log_level * 10),
+        format='%(asctime)s [%(levelname)s] %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S')
 
-    shutil.copytree(PUBLIC_DIR, os.path.join(OUTPUT_DIR, "public"))
+    if args.watch:
+        watch(args.source, args.output)
+        return
+
+    rebuild(args.source, args.output)
+
+
+def watch(source_dir: str, output_dir: str):
+    logging.info(f"Start watching {source_dir} ...")
+    for changes in watchfiles.watch(source_dir, raise_interrupt=False):
+        for (m, f) in changes:
+            logging.info(f"Change detected: {m.name} {f}")
+        rebuild(source_dir, output_dir)
+
+
+def rebuild(source_dir: str, output_dir: str):
+    public_dir = os.path.join(source_dir, "public")
+    pages_dir = os.path.join(source_dir, "pages")
+    template_dir = os.path.join(source_dir, "templates")
+
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+
+    shutil.copytree(public_dir, os.path.join(output_dir, "public"))
 
     pages = []
-    for filename in sorted(os.listdir(PAGES_DIR)):
+    for filename in sorted(os.listdir(pages_dir)):
         name = strip_before_first(strip_after_last(filename, '.'), '_')
-        pages.append((name, os.path.join(PAGES_DIR, filename)))
-
-    print("Pages:", pages)
+        pages.append((name, os.path.join(pages_dir, filename)))
 
     for (pagename, pagepath) in pages:
         pagecontent = get_file_contents(pagepath)
         parsedcontent = parse_templates(
-            pagecontent, pagename, [p for (p, _) in pages])
+            pagecontent, pagename, [p for (p, _) in pages], template_dir)
 
-        outpath = os.path.join(OUTPUT_DIR, pagename, "index.html")
+        outpath = os.path.join(output_dir, pagename, "index.html")
         if pagename in OUTPUT_OVERRIDES:
-            outpath = os.path.join(OUTPUT_DIR, OUTPUT_OVERRIDES[pagename])
+            outpath = os.path.join(output_dir, OUTPUT_OVERRIDES[pagename])
 
         outdir = os.path.dirname(outpath)
         if not os.path.exists(outdir):
@@ -53,7 +88,7 @@ def main():
             out.write(parsedcontent)
 
 
-def parse_templates(content: str, pagename: str, pages: list[str]):
+def parse_templates(content: str, pagename: str, pages: list[str], template_dir: str):
     template = find_next_template(content)
 
     while True:
@@ -69,11 +104,11 @@ def parse_templates(content: str, pagename: str, pages: list[str]):
             content = content[:start] + content[end+1:]
             ext_template = args[0]
             template_contents = get_file_contents(
-                TEMPLATE_DIR, f"{ext_template}.html")
+                template_dir, f"{ext_template}.html")
             (start, end, _) = find_template(template_contents, "pagecontent")
             content = template_contents[:start] + \
                 content + template_contents[end+1:]
-            content = parse_templates(content, pagename, pages)
+            content = parse_templates(content, pagename, pages, template_dir)
         elif id == "pagename":
             name = pagename
             if pagename in NAV_OVERRIDES:
