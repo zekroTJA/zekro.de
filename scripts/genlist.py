e3b3e760edde90b2abab1d21337528d5aac21290
zekroTJA <riho@live.de>
2025-01-24 14:24:57 +0100

    update photography page

--- scripts/genlist.py
+++ scripts/genlist.py
@@ -3,18 +3,22 @@
 import yaml
 
 
+def id(name: str):
+    return name.lower().replace(" ", "-")
+
+
 def main():
     with open(0, encoding="utf-8") as f:
         data = yaml.safe_load(f)
 
     for list in data:
-        print("<section>")
-        print(f"<h3>{list["title"]}</h3>")
+        print(f'<section id="{id(list["title"])}">')
+        print(f'<h3>{list["title"]}</h3>')
         print("<ul>")
         for item in list["items"]:
             if link := item.get("link"):
-                print(f'<li><a target="_blank" rel="noopener" href="{
-                      link}">{item["name"]}</a></li>')
+                print('<li><a target="_blank" rel="noopener" '
+                      f'href="{link}">{item["name"]}</a></li>')
             else:
                 print(f'<li>{item["name"]}</li>')
         print("</ul>")
